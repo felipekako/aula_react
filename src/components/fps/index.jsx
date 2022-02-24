b086d397d055231c84ab1f9bd59c51906d1ab637
felipekako <felipekakosampaio@gmail>
2022-02-24 10:58:34 -0300

    subindo a branch fps

--- src/components/fps/index.jsx
+++ src/components/fps/index.jsx
@@ -1,17 +1,19 @@
 import fbs from '../imagens/fbs.jpg';
+import lego from '../imagens/fps_lego.jpg';
 
 const Fps = () =>{
     return(
         <>
         <img src= {fbs}/>
-        
+        <img src= {lego}/>
+        <img src= {lego}/>
        
         </>    
     );
 }
 
 export default Fps;
-
+ 
 
 
 
